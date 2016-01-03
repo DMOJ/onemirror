2f5cc5d5e3761c78f485b1d91b507cddbadb43a6
Quantum <quantum2048@gmail.com>
2016-01-02 22:48:16 -0500

    Use argparse

--- onemirror/main.py
+++ onemirror/main.py
@@ -1,9 +1,19 @@
+import argparse
+
 from onemirror.database import OneDriveDatabaseManager
 
 
 def main():
-    with OneDriveDatabaseManager('onedrive.db', '000000004C17987A', 'xk9GckVE6ZUM-rgSmjDx8JuTNvWLXdV3') \
-            as (store, client):
+    parser = argparse.ArgumentParser(description='OneDrive mirroring program: create a complete '
+                                                 'mirror of OneDrive contents')
+    parser.add_argument('remote', nargs='?', default='/')
+    parser.add_argument('local', nargs='?', default='OneDrive')
+    parser.add_argument('database', nargs='?', default='onedrive.db')
+    parser.add_argument('--client-id', default='000000004C17987A')
+    parser.add_argument('--secret', default='xk9GckVE6ZUM-rgSmjDx8JuTNvWLXdV3')
+    args = parser.parse_args()
+
+    with OneDriveDatabaseManager(args.database, args.client_id, args.secret) as (store, client):
         for i in client.view_delta('/'):
             __import__('pprint').pprint(i)
 
